257afe49ffcff730a150c48111a5c62b9030256e
DBIS\jonathan.turner <jonathan.turner@tba.group>
2017-12-06 15:44:25 +0000

    Add project files.

---
+++ CommTracMobile/CommTracMobile.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CommTracMobile.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new CommTracMobile.App());
+        }
+    }
+}
